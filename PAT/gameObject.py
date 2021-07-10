7175fb232ba705dd239999e2b4ea8b42ed21d2d2
justinzhang <justinz@andrew.cmu.edu>
2021-07-10 17:59:24 -0400

    Imported game objects from my previous project. Will put into the classes.py file once tested.
    Some considerations still to be made, specifically how acceleration will be used,
    if more functionality is needed, and to add out of bound object management

---
+++ PAT/gameObject.py
@@ -0,0 +1,43 @@
+#Justin Zhang's imported game object class
+import pygame
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self,coord,imgName,velocity = 0,acceleration = 0):
+        self.x,self.y = coord
+        self.vel = velocity
+
+        #should we have acceleration?
+        self.acc = acceleration
+
+        #add additional argument if resize is needed
+        self.image = self.imgLoad(imgName)
+        
+        #store the dimension of the sprite
+        self.xDim,self.yDim = self.image.get_rect().size
+
+        #pygame uses Rect to detect collisions
+        #everytime the object moves, we need to update the Rect coordinates (call setRect whenever object pos changes)
+        self.setRect()
+
+    #load all the images at once so it doesnt need to be done constantly
+    #must be in the game image directory
+    #default size will be 80x80
+    @staticmethod
+    def imgLoad(img,resizeDim = (80,80)):
+        playerImg = pygame.image.load(f"images/objects/{img}").convert_alpha()
+        playerImg = pygame.transform.scale(playerImg,resizeDim)
+        return playerImg
+
+    
+
+    def setRect(self):
+        self.rect = pygame.Rect(self.x - self.xDim/2,self.y - self.yDim/2,
+                                self.xDim,self.yDim)
+
+    #direction is horizontal, then veritical
+    def move(self,horizontal = False, vertical = False):
+        self.x += horizontal * self.vel
+        self.y += vertical * self.vel
+
+        #TODO: check for out of bounds
+        self.setRect()
