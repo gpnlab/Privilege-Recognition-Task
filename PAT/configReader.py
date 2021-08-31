2f6d9cd1d0f3d5e5f5380539040f224203fa7a79
justinzhang <justinz@andrew.cmu.edu>
2021-08-31 11:06:30 -0400

    commiting all

--- PAT/configReader.py
+++ PAT/configReader.py
@@ -1,8 +1,10 @@
 from os import path
+from toExe import EXE
 import json
 class ConfigReader:
     def parseToDict(filename):
-        with open(path.join(path.dirname(__file__),f"config/{filename}"),"r+") as config:
+        url = EXE.resource_path(f"config/{filename}")
+        with open(url,"r+") as config:
             d = json.load(config)
         print(f"loaded config {filename}")
 
